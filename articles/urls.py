b0d03945b67aca89d00f0cc40e3cc173c3a49b84
amd2002 <mamadousaidoubarryaid@gmail.com>
2024-09-11 12:40:53 +0000

    modification du setting.py

--- articles/urls.py
+++ articles/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import list_article, create_article,edit_article,delete_article,article_detail
+from .views import list_article, create_article, edit_article, delete_article, article_detail
 
 urlpatterns = [
-    path('', list_article, name='list_articles'),
-    path('nouveau/', create_article, name='create_article'),
-    path('edit/<int:id>/',edit_article, name ='edit_article'),
-    path ('delete/<int:id>/', delete_article, name='delete_article'),
-    path('<int:id>/', article_detail, name='article_detail'),  
+    path('', list_article, name='list_articles'),  # Liste des articles
+    path('nouveau/', create_article, name='create_article'),  # Création d'un article
+    path('edit/<int:id>/', edit_article, name='edit_article'),  # Modification d'un article
+    path('delete/<int:id>/', delete_article, name='delete_article'),  # Suppression d'un article
+    path('<int:id>/', article_detail, name='article_detail'),  # Détails d'un article
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,43 +1,32 @@
-#views.py
-from django.shortcuts import render,redirect, get_object_or_404
-from.models import Article, Comment    # Importation des modèles Article et Comment 
-from .forms import ArticleForm, CommentForm        # Importation des fonctions pour rendre les templates et gérer les erreurs 404
-
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Article, Comment  # Importation des modèles Article et Comment
+from .forms import ArticleForm, CommentForm  # Importation des formulaires
 
 def list_article(request):
-    arts = Article.objects.all()
-
-    context= {
-        'articles': arts
-    }
+    arts = Article.objects.all()  # Récupération de tous les articles
+    context = {'articles': arts}
     return render(request, 'articles/list_articles.html', context)
-
 
 def create_article(request):
     if request.method == 'POST':
-        form = ArticleForm(request.POST, request.FILES)  # Gère les fichiers téléchargés avec request.FILES
+        form = ArticleForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect('list_articles')
-        else:
-            print(form.errors)  # Affiche les erreurs du formulaire dans la console (pour déboguer)
     else:
         form = ArticleForm()
-    
     return render(request, 'articles/formulaire.html', {'form': form})
-
 
 def edit_article(request, id):
     article = get_object_or_404(Article, id=id)
     if request.method == 'POST':
-        form = ArticleForm(request.POST, request.FILES, instance=article)  # Gère les fichiers téléchargés avec request.FILES
+        form = ArticleForm(request.POST, request.FILES, instance=article)
         if form.is_valid():
             form.save()
             return redirect('list_articles')
     else:
         form = ArticleForm(instance=article)
     return render(request, 'articles/edit_article.html', {'form': form})
-
 
 def delete_article(request, id):
     article = get_object_or_404(Article, id=id)
@@ -46,26 +35,18 @@ def delete_article(request, id):
         return redirect('list_articles')
     return render(request, 'articles/delete_article.html', {'article': article})
 
-
-from django.shortcuts import render, get_object_or_404  # Importation des fonctions pour rendre les templates et gérer les erreurs 404
-from .models import Article, Comment  # Importation des modèles Article et Comment
-from .forms import CommentForm  # Importation du formulaire CommentForm
-
-# Vue pour afficher les détails d'un article spécifique
 def article_detail(request, id):
     article = get_object_or_404(Article, id=id)
     comments = article.comments.filter(active=True)  # Récupération des commentaires actifs
-
     if request.method == 'POST':
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             new_comment = comment_form.save(commit=False)
             new_comment.article = article
             new_comment.save()
-            return redirect('article_detail', id=article.id)  # Redirection après soumission
+            return redirect('article_detail', id=article.id)
     else:
         comment_form = CommentForm()
-
     return render(request, 'articles/article_detail.html', {
         'article': article,
         'comments': comments,
