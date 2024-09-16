373ae7999b4d474209da22d35329d00677168489
amd2002 <mamadousaidoubarryaid@gmail.com>
2024-09-16 16:21:21 +0000

    modificatin

--- blog/settings.py
+++ blog/settings.py
@@ -1,12 +1,16 @@
-from pathlib import Path
 import os
+from pathlib import Path
+
+# Chargement des variables d'environnement
+from dotenv import load_dotenv
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-b2fp-!&_hjo#7llrfl*ujh*$gelefjwrf6^l=^1rtgt5y1is!^'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-b2fp-!&_hjo#7llrfl*ujh*$gelefjwrf6^l=^1rtgt5y1is!^')
 
-DEBUG = False
-ALLOWED_HOSTS = ['massaid2002.pythonanywhere.com','localhost', '127.0.0.1']
+DEBUG = True
+ALLOWED_HOSTS = ['massaid2002.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -18,6 +22,19 @@ INSTALLED_APPS = [
     'articles', 
     'users',
 ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blogdb',
+        'USER': 'root',  # Utilisateur par défaut pour XAMPP/MySQL
+        'PASSWORD': '',  # Laisse vide si pas de mot de passe pour root
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -49,13 +66,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'blog.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -86,3 +96,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'list_articles'
 LOGOUT_REDIRECT_URL = 'login'
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
